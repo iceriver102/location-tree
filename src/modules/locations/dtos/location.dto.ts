614e953331e8e79fae536953516204adb6b4dae0
Phan Thanh Giang <giang.phan@alta.com.vn>
2024-10-14 10:16:43 +0700

    fix: document of api

--- src/modules/locations/dtos/location.dto.ts
+++ src/modules/locations/dtos/location.dto.ts
@@ -1,31 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
 
 export class LocationDTO {
     @Expose()
+    @ApiProperty({description: "The id of node", type:"string"})
     id: string;
 
+    @ApiProperty({description: "The name of node", example:"Telco"})
     @Expose()
     name: string;
 
+    @ApiProperty({description: "The level of node"})
     @Expose()
     level: string;
 
+    @ApiProperty({description: "The location number of node"})
     @Expose()
     locationNumber: string;
     
+    @ApiProperty({description: "The location area of node", type: "number"})
     @Expose()
     locationArea: number;
 
+    @ApiProperty({description: "The location building of node"})
     @Expose()
     locationBuilding?: string;
 
-    @Expose()
-    @Type(() => LocationDTO)
-    children: LocationDTO[];
 
+    @ApiProperty({description: "The location children", type: LocationDTO, isArray: true})
     @Expose()
     @Type(() => LocationDTO)
-    parent: LocationDTO;
+    children?: LocationDTO[];
+
+    @ApiProperty({description: "The location parent",  type: LocationDTO})
+    @Expose()
+    @Type(() => LocationDTO)
+    parent?: LocationDTO;
 
 
 }
--- src/modules/locations/locations.controller.ts
+++ src/modules/locations/locations.controller.ts
@@ -8,7 +8,7 @@ import {
     Delete,
 } from '@nestjs/common';
 import { Serialize } from '~core/interceptors/serialize.interceptor';
-import { ApiTags } from '@nestjs/swagger';
+import {  ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LocationService } from './services/location.service';
 import { LocationDTO } from './dtos/location.dto';
 import { CreateLocationDTO } from './dtos/create-location.dto';
@@ -22,25 +22,34 @@ export class LocationsController {
     ) { }
 
     @Post("/addNode")
+    @ApiOperation({ summary: 'Create a node of location tree', description: "The api create a node of location tree if the parent id is null the node is root" })
+    @ApiResponse({ type: LocationDTO })
     @Serialize(LocationDTO)
     public async create(@Body() body: CreateLocationDTO) {
         return this.locationService.create(body, body.parentId)
     }
 
     @Get("/tree")
+    @ApiOperation({ summary: 'View location tree' })
+    @ApiResponse({ type: LocationDTO })
     @Serialize(LocationDTO)
     public async getTree() {
         return this.locationService.findTrees();
     }
 
     @Patch("/node/:id")
-    public async updateNode(@Param("id") id:string, @Body() body: UpdateLocationDto) {
+    @ApiOperation({ summary: 'Update information a node of location tree' })
+    @ApiParam({ name: "id", description: "The id of node" })
+    public async updateNode(@Param("id") id: string, @Body() body: UpdateLocationDto) {
         return this.locationService.updateNode(id, body);
     }
 
     @Delete("/node/:id")
+    @ApiResponse({ type: LocationDTO })
+    @ApiParam({ name: "id", description: "The id of node" })
+    @ApiOperation({ summary: 'Delete a node and subtree of location tree' })
     @Serialize(LocationDTO)
-    public async deleteNode(@Param("id") id:string) {
+    public async deleteNode(@Param("id") id: string) {
         return this.locationService.remove(id);
     }
 
